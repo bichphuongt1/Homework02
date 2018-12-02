8aab872f1230a771ba1e401342262ff84d98867d
bichphuongt1 <bomkhec@gmail.com>
2018-12-02 15:34:05 +0700

    update changePassword without softAssert

--- src/test/java/test/password/ChangePasswordTest.java
+++ src/test/java/test/password/ChangePasswordTest.java
@@ -3,7 +3,6 @@ package test.password;
 import static org.testng.Assert.assertEquals;
 
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import constant.Constant;
 import constant.TabMenu;
