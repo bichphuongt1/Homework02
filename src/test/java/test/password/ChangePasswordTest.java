b69c75d1082e82cbe091f87b013ead8a417bf5b6
bichphuongt1 <bomkhec@gmail.com>
2018-12-02 15:31:28 +0700

    update changePassword without softAssert

--- src/test/java/test/password/ChangePasswordTest.java
+++ src/test/java/test/password/ChangePasswordTest.java
@@ -17,17 +17,14 @@ public class ChangePasswordTest extends TestBase {
 	LoginPage loginPage = new LoginPage();
 	HomePage homePage = new HomePage();
 	ChangePasswordPage changePasswordPage = new ChangePasswordPage();
-	SoftAssert softAssert = new SoftAssert();
 	
 	@Test(description = "TC09 - User can change password.")
 	public void TC09() {
 		homePage.gotoPage(TabMenu.LOGIN);
-		loginPage.login(Constant.UserInfo.USERNAME_USING_CHANGE_PASSWORD, Constant.UserInfo.PASSWORD, 1);
+		loginPage.login(Constant.UserInfo.USERNAME_USING_CHANGE_PASSWORD, Constant.UserInfo.PASSWORD, Constant.LOGIN_ONE_TIME);
 		homePage.gotoPage(TabMenu.CHANGE_PASSWORD);
 		changePasswordPage.changePassword(Constant.UserInfo.PASSWORD, Constant.UserInfo.NEW_PASSWORD, Constant.UserInfo.NEW_PASSWORD);
 		assertEquals(changePasswordPage.getChangePasswordSuccessMessage(), Constant.Message.CHANGE_PASSWORD_SUCCESS_MSG);
-		softAssert.assertEquals(changePasswordPage.getChangePasswordSuccessMessage(), Constant.Message.CHANGE_PASSWORD_SUCCESS_MSG);
 		changePasswordPage.changePassword(Constant.UserInfo.NEW_PASSWORD, Constant.UserInfo.PASSWORD, Constant.UserInfo.PASSWORD);
-		softAssert.assertAll();
 	}
 }
